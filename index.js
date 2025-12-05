2937a65a44e00073bae604768345ebf883bb4a1c
sharifulislamudoy <surifroton301@gmail.com>
2025-12-05 22:00:11 +0600

    done

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://course-master-weld.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
